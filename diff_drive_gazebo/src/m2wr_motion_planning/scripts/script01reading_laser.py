275eff88b138edf25f043f930af7ed5c83a0efb9
Kaustav Ghosh <teetangh@gmail.com>
2020-07-06 12:55:16 +0530

    sciprt errors to be fixed

--- diff_drive_gazebo/src/m2wr_motion_planning/scripts/script01reading_laser.py
+++ diff_drive_gazebo/src/m2wr_motion_planning/scripts/script01reading_laser.py
@@ -1,16 +1,14 @@
-#! /usr/bin/env python
-
 import rospy
 
-from sensor.msgs import LaserScan
+from sensor_msgs.msgs import LaserScan
 
 def clbk_laser(msgs):
 	rospy.loginfo(msgs)
 
 def main():
 	rospy.init('reading_laser')
-
 	sub = rospy.Subsriber('/m2wr/laser/scan',LaserScan,clbk_laser)
+	rospy.spin()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 	main()
