9988bcd9ef01d5baf5c1a0e336cc789a72deebc4
Kaustav Ghosh <teetangh@gmail.com>
2020-07-06 12:20:06 +0530

    after hours finally world.launch working

--- diff_drive_gazebo/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kaustav/Qbotics-Labs-Internship/diff_drive_gazebo/src
-# Build directory: /home/kaustav/Qbotics-Labs-Internship/diff_drive_gazebo/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("m2wr_description")
