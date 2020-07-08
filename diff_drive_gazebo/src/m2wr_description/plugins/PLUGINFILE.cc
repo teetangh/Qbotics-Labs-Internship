1446a3cc3d8d9f56d25e60f451843115c1b4a741
Kaustav Ghosh <teetangh@gmail.com>
2020-07-08 12:24:21 +0530

    added lazdar lidar but ROS api not working

---
+++ diff_drive_gazebo/src/m2wr_description/plugins/PLUGINFILE.cc
@@ -0,0 +1,81 @@
+// #ifndef LAZ_SENSOR_PLUGIN
+// #define LAS_SENSOR_PLUGIN
+
+// // Include Gazebo headers.
+
+// #include <gazebo/gazebo.hh>
+// #include <gazebo/physics/physics.hh>
+// #include <gazebo/plugins/RayPlugin.hh>
+// #include <gazebo/sensors/Sensor.hh>
+// #include <gazebo/sensors/SensorTypes.hh>
+// #include <gazebo/sensors/RaySensor.hh>
+
+// // Include ROS headers so we can communicate with our robot
+// #include <ros/ros.h>
+// #include <ros/console.h>
+
+// // Include std::string's because they're pretty darn useful.
+// #include <string>
+
+// // So we don't have to do gazebo::everything
+// namespace gazebo
+// {
+//     // Defining our plugin class
+//     class LazSensorPlugin : public RayPlugin
+//     {
+//     private:
+//         sensors::RaySensorPtr _sensor;
+//         physics::LinkPtr _l;
+
+//     public:
+//         LazSensorPlugin() {}
+
+//         // Runs when the model is loaded
+//         virtual void Load(sensors::SensorPtr _s, sdf::ElementPtr _sdf)
+//         {
+//             if (!ros::isInitialized())
+//             {
+//                 ROS_FATAL_STREAM("ROS node for Gazebo not established. Plugin failed.");
+//                 return;
+//             }
+//             RayPlugin::Load(_s, _sdf);
+
+//             std::string parentName = _sensor->ParentName();
+//             std::string modelName = "";
+//             std::string linkName = "";
+//             modelName = parentName.substr(0, parentName.find(':'));
+//             linkName = parentName.substr(parentName.find(':') + 2, parentName.size());
+
+//             physics::WorldPtr _w = physics::get_world(_sensor->WorldName());
+//             physics::ModelPtr _m = _w->GetModel(modelName.c_str());
+//             _l = _m->GetLink(linkName);
+
+//             _sensor = std::dynamic_pointer_cast<sensors::RaySensor>(_s);
+//             ROS_INFO("Laser Sensor Plugin Loaded");
+//         }
+
+//         virtual void OnNewLaserScans()
+//         {
+//             double yaw = _sensor->Pose().Rot().Yaw() + _l->GetRelativePose().rot.GetYaw();
+//             std::string out = "";
+//             std::vector<double> ranges;
+//             _sensor->Ranges(ranges);
+//             for (int i = 0; i < ranges.size(); i++)
+//             {
+//                 out = out + "[";
+//                 out = out + std::to_string(i);
+//                 out = out + "] ";
+//                 out = out + std::to_string(ranges[i]);
+//                 out = out + ", ";
+//             }
+
+//             ROS_INFO("%s", yaw, out.c_str());
+//             ROS_INFO("[%f] %s", yaw, out.c_str());
+//         };
+//     };
+
+//     // Gazebo macro to set up the rest of the plugin functionality
+//     GZ_REGISTER_SENSOR_PLUGIN(LazSensorPlugin)
+// } // namespace gazebo
+
+// #endif
