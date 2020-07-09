f6173d0dcbbb1cf478bb3b8fbf9691b9b3ee7f62
Kaustav Ghosh <teetangh@gmail.com>
2020-07-09 18:00:09 +0530

    finally installed protobuf v3 - fixed some errors

---
+++ diff_drive_gazebo/build/velodyne_simulator/velodyne_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kaustav/Qbotics-Labs-Internship/diff_drive_gazebo/src/velodyne_simulator/velodyne_description
+# Build directory: /home/kaustav/Qbotics-Labs-Internship/diff_drive_gazebo/build/velodyne_simulator/velodyne_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
