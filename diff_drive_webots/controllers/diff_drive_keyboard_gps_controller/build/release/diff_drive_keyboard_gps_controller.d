bb416f232702f3ce01415529e46752d6246d1c71
Kaustav Ghosh <teetangh@gmail.com>
2020-07-12 21:29:45 +0530

    added GPS integration to diff drive with code

---
+++ diff_drive_webots/controllers/diff_drive_keyboard_gps_controller/build/release/diff_drive_keyboard_gps_controller.d
@@ -0,0 +1,9 @@
+build/release/diff_drive_keyboard_gps_controller.o: \
+ diff_drive_keyboard_gps_controller.cpp \
+ /usr/local/webots/include/controller/cpp/webots/DistanceSensor.hpp \
+ /usr/local/webots/include/controller/cpp/webots/Device.hpp \
+ /usr/local/webots/include/controller/cpp/webots/../../c/webots/types.h \
+ /usr/local/webots/include/controller/cpp/webots/Motor.hpp \
+ /usr/local/webots/include/controller/cpp/webots/Robot.hpp \
+ /usr/local/webots/include/controller/cpp/webots/Keyboard.hpp \
+ /usr/local/webots/include/controller/cpp/webots/GPS.hpp
