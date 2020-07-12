221bc69edbd66bd927a75ffc6cf64070b99339fb
Kaustav Ghosh <teetangh@gmail.com>
2020-07-12 21:02:26 +0530

    added keyboard teleoperation

---
+++ diff_drive_webots/controllers/diff_drive_keyboard_teleoperation_controller/build/release/diff_drive_keyboard_teleoperation_controller.d
@@ -0,0 +1,8 @@
+build/release/diff_drive_keyboard_teleoperation_controller.o: \
+ diff_drive_keyboard_teleoperation_controller.cpp \
+ /usr/local/webots/include/controller/cpp/webots/DistanceSensor.hpp \
+ /usr/local/webots/include/controller/cpp/webots/Device.hpp \
+ /usr/local/webots/include/controller/cpp/webots/../../c/webots/types.h \
+ /usr/local/webots/include/controller/cpp/webots/Motor.hpp \
+ /usr/local/webots/include/controller/cpp/webots/Robot.hpp \
+ /usr/local/webots/include/controller/cpp/webots/Keyboard.hpp
---
+++ diff_drive_webots/controllers/diff_drive_keyboard_teleoperation_controller/diff_drive_keyboard_teleoperation_controller.cpp
@@ -0,0 +1,82 @@
+#include <webots/DistanceSensor.hpp>
+#include <webots/Motor.hpp>
+#include <webots/Robot.hpp>
+#include <webots/Keyboard.hpp>
+
+#define TIME_STEP 64
+using namespace webots;
+
+int main(int argc, char const *argv[])
+{
+  Robot *robot = new Robot();
+  Keyboard kb;
+  DistanceSensor *ds[2];
+  char dsNames[2][50] = {"diff_drive_laser_scanner_right", "diff_drive_laser_scanner_left"};
+
+  for (int i = 0; i < 2; i++)
+  {
+    ds[i] = robot->getDistanceSensor(dsNames[i]);
+    ds[i]->enable(TIME_STEP);
+  }
+
+  Motor *wheels[4];
+  char wheels_names[4][50] = {
+      "diff_drive_wheel1_device",
+      "diff_drive_wheel2_device",
+      "diff_drive_wheel3_device",
+      "diff_drive_wheel4_device",
+  };
+
+  for (int i = 0; i < 4; i++)
+  {
+    wheels[i] = robot->getMotor(wheels_names[i]);
+    wheels[i]->setPosition(INFINITY);
+    wheels[i]->setVelocity(0.0);
+  }
+
+  kb.enable(TIME_STEP);
+  double leftSpeed = 0.0;
+  double rightSpeed = 0.0;
+
+  while (robot->step(TIME_STEP) != -1)
+  {
+    int key = kb.getKey();
+
+    if (key == 315)
+    {
+      leftSpeed = 1.0;
+      rightSpeed = 1.0;
+    }
+    else if (key == 317)
+    {
+      leftSpeed = -1.0;
+      rightSpeed = -1.0;
+    }
+    else if (key == 316)
+    {
+      leftSpeed = 1.0;
+      rightSpeed = -1.0;
+    }
+    else if (key == 314)
+    {
+      leftSpeed = -1.0;
+      rightSpeed = 1.0;
+    }
+    else
+    {
+      leftSpeed = 0;
+      rightSpeed = 0;
+    }
+
+    std::cout << ds[0]->getValue() << "=Right Sensor" << std::endl;
+    std::cout << ds[1]->getValue() << "=Left Sensor" << std::endl;
+
+    wheels[0]->setVelocity(leftSpeed);
+    wheels[1]->setVelocity(rightSpeed);
+    wheels[2]->setVelocity(leftSpeed);
+    wheels[3]->setVelocity(rightSpeed);
+  }
+
+  delete robot;
+  return 0;
+}
